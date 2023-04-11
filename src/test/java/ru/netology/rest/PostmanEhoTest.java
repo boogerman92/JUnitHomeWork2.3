34cddc38d8cc0781a1cfffbadd543b6b67ac2b22
Nazarenko A A <boogerman1992@gmail.com>
2023-04-11 10:49:39 +0400

    Artifacts

---
+++ src/test/java/ru/netology/rest/PostmanEhoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+
+public class PostmanEhoTest {
+    @Test
+    void ehoText() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
